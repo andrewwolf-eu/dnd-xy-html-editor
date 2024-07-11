824f2acf6838a8d0ae4363311286deac917bc1de
Farkas András <farkas.andras.uk@gmail.com>
2024-07-11 13:05:01 +0200

    Updated story config

--- stories/DndXYHtmlEditor.stories.tsx
+++ stories/DndXYHtmlEditor.stories.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 import { DndXYHtmlEditor } from "../src/DndXYHtmlEditor";
@@ -6,7 +7,7 @@ const meta = {
   title: "Demo/DndXYHtmlEditor",
   component: DndXYHtmlEditor,
   parameters: {
-    layout: "centered",
+    layout: "fullscreen",
   },
   tags: ["autodocs"],
   argTypes: {
@@ -19,8 +20,10 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Demo: Story = {
-  args: {
-    primary: true,
-    label: "DndXYHtmlEditor",
-  },
+  render: (args) => (
+    <div style={{ width: '100%', height: '100%' }}>
+      <DndXYHtmlEditor />
+    </div>
+  ),
+  args: {},
 };
